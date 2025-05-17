a77078e444367207fe00f30ff29442f2ec1046b3
flosamuu <flosamuu@gmail.com>
2025-05-17 14:30:09 -0400

    Comments and changed if to while in case user misspells the file path or smt

--- fileWriting.java
+++ fileWriting.java
@@ -1,3 +1,7 @@
+// This thing is supposed to create a text file with every odd line of another file
+// Default input file will be classTextFile.txt. If it doesn't exist, enter the path to whatever input file you want
+// It will create a new file, classTextFileTwo.txt, in your desktop to output the odd lines to
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -11,17 +15,18 @@ public class fileWriting {
         // Prerequisite: the file classTextFile.txt exists
         File inputFile = new File(home + "/Desktop/classTextFile.txt");
 
-        // Creates a file called classTextFileTwo
+        // Creates a file called classTextFileTwo to put the output in
         File outputFile = new File(home + "/Desktop/classTextFileTwo.txt");
         
         Scanner input = new Scanner(System.in);
 
-        if (!inputFile.exists()) {
+        while (!inputFile.exists()) {
             System.out.println("Erm input file not found at " + inputFile.getAbsolutePath());
             System.out.print("Enter the full path to the input file: ");
             String path = input.nextLine();
             
             inputFile = new File(path);
+            System.out.println();
         }
         
         input.close();
@@ -30,7 +35,7 @@ public class fileWriting {
         // String to store the text we want
         String fileContent = "";
 
-        // Make a new file containing every odd line from the input file
+        // Add every odd line from the input file to the string
         int lineNumber = 1;
         while (scan.hasNextLine()) {
             String line = scan.nextLine();
@@ -42,11 +47,12 @@ public class fileWriting {
 
         scan.close();
 
+        // Write that in 🗣🗣📝!!
         FileWriter writer = new FileWriter(outputFile);
-
 
         writer.write(fileContent);
         writer.close();
+
         System.out.print("yippee! Output written to " + outputFile.getAbsolutePath());
     }
 }
